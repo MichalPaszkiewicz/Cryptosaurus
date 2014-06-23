0d6a35246bc03273df72af3ee098786a8df6aed8
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-06-23 14:35:39 +0100

    Calculating score with vigenere cracker

--- Scripts/Custom scripts/VigenereCracker.js
+++ Scripts/Custom scripts/VigenereCracker.js
@@ -1,4 +1,44 @@
 ﻿function crackVigenereCipher(input, length) {
-    return "muhahaha cracked";
+    return getScore(input);
+}
+
+function getWords() {
+    var request = new XMLHttpRequest();
+    request.open("GET", "Scripts/Custom scripts/WordList/brit-a-z.txt", false);
+    request.send(null);
+    var returnValue = request.responseText.replace(" ", "");
+
+    var removeCarriageReturn = returnValue;
+
+    var words = removeCarriageReturn.split("\r\n");
+
+    var len = words.length;
+
+    for (var i = 0; i < len; i++) {
+        words[i].replace("\n", "").replace("\r", "");
+    }
+
+    for (i = 0; i < len; i++) {
+        words[i] && words.push(words[i]); // copy non-empty values to the end of the array
+    }
+
+    words.splice(0, len);
+
+    return words;
+}
+
+function getScore(input) {
+    var words = getWords();
+
+    var score = 0;
+
+    for (var i = 0; i < words.length; i++) {
+        var word = words[i];
+        if (input.toLowerCase().indexOf(word) > -1) {
+            score++;
+        }
+    }
+
+    return score;
 }
 //# sourceMappingURL=VigenereCracker.js.map
--- Scripts/Custom scripts/VigenereCracker.ts
+++ Scripts/Custom scripts/VigenereCracker.ts
@@ -1,5 +1,45 @@
-﻿function crackVigenereCipher(input, length) {
+﻿function crackVigenereCipher(input :string, length) {
 
+    return getScore(input);
+}
 
-    return "muhahaha cracked";
+function getWords() {
+
+    var request = new XMLHttpRequest();
+    request.open("GET", "Scripts/Custom scripts/WordList/brit-a-z.txt", false);
+    request.send(null);
+    var returnValue = request.responseText.replace(" ", "");
+
+    var removeCarriageReturn = returnValue;
+
+    var words = removeCarriageReturn.split("\r\n");
+
+    var len = words.length;
+
+    for (var i = 0; i < len; i++) {
+        words[i].replace("\n", "").replace("\r", "");
+    }
+
+    for (i = 0; i < len; i++) {
+        words[i] && words.push(words[i]);  // copy non-empty values to the end of the array
+    }
+
+    words.splice(0, len);
+
+    return words;
+}
+
+function getScore(input :string) {
+    var words = getWords();
+
+    var score = 0;
+
+    for (var i = 0; i < words.length; i++) {
+        var word = words[i];
+        if (input.toLowerCase().indexOf(word) > -1) {
+            score++;
+        }
+    }
+
+    return score;
 }
